66624abade35bfbe46fd57e4a38102f97a6ea468
Pabolt84 <>
2025-01-24 23:24:14 +0100

    Bubble check and colision

--- GGJ25/Assets/Pablo/Scripit/BillboardController.cs
+++ GGJ25/Assets/Pablo/Scripit/BillboardController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BillboardController : MonoBehaviour
 {
@@ -9,5 +10,10 @@ public class BillboardController : MonoBehaviour
         transform.LookAt(Camera.main.transform.position);
         transform.rotation = Quaternion.LookRotation(Camera.main.transform.position - transform.position);
         transform.Rotate(180, 0, 180);
+    }
+
+    public void ChangeImagen(Sprite image)
+    {
+        transform.GetChild(0).GetChild(0).GetComponent<Image>().sprite = image;
     }
 }
--- GGJ25/Assets/Pablo/Scripit/PlantControler.cs
+++ GGJ25/Assets/Pablo/Scripit/PlantControler.cs
@@ -5,27 +5,48 @@ using UnityEngine;
 public class PlantControler : MonoBehaviour
 {
     public int Estado;
-    public List<Material> Sprites;
+    public List<Sprite> Sprites;
     private BoxCollider ColliderPlant;
     private BillboardController Body;
+    private Rigidbody rig_;
 
     // Start is called before the first frame update
     void Start()
     {
         Estado = 0;
         ColliderPlant = GetComponent<BoxCollider>();
-        Body = GetComponent<BillboardController>();
-        
+        Body = GetComponentInChildren<BillboardController>();
+        rig_ = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Body.GetComponent<MeshRenderer>().material = Sprites[Estado];
+        if (Estado < 0)
+        {
+            Estado = 0;
+        }
+
+        Body.ChangeImagen(Sprites[Estado]);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Bubble"))
+        {
+            Debug.Log("Hello");
+
+            BubbleMovement avg = other.GetComponent<BubbleMovement>();
+
+            if(avg.Type- 1 == Estado)
+            {
+                Estado++;
+            }else if(avg.Type < 0)
+            {
+                Estado += avg.Type;
+            }
+
+            Destroy(other);
+        }  
     }
 }
