e23d7d6010db93a76d5b5f225f1ecb2d080b4a28
Andreu Llàcer <llacer.andreu@gmail.com>
2025-01-25 00:31:02 +0100

    Sombras de burbujas

--- GGJ25/Assets/Metxa/0_Scripts/BubbleMovement.cs
+++ GGJ25/Assets/Metxa/0_Scripts/BubbleMovement.cs
@@ -1,17 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BubbleMovement : MonoBehaviour
 {
     public int Type = 0, BadnessChance = 5,
         DownSpeed = 2,  SideSpeed = 8;
     public bool Direction = true; // true = derecha / false = izquierda
+    public List<Sprite> BubbleSprites = new List<Sprite>();
+    public List<Sprite> ItemSprites = new List<Sprite>();
+    public float GroundY = -5;
+    BillboardController BC;
+    private GameObject Shadow;
+    private float ShadowYValue;
+
     void Start()
     {
         Type = Random.Range(1, 4);
-        if(Random.Range(1, 10) < BadnessChance)
+
+        BC = transform.GetChild(0).GetComponent<BillboardController>();
+
+        if (Random.Range(1, 10) < BadnessChance)
         {
+            BC.ChangeImagen(BubbleSprites[Random.Range(3, 6)]);
             Type *= -1;
             if (Type == -3)
             {
@@ -19,15 +31,24 @@ public class BubbleMovement : MonoBehaviour
                 else Type--;
             }
         }
+        else
+        {
+            BC.ChangeImagen(BubbleSprites[Random.Range(0, 3)]);
+        }
 
         if (Random.Range(0,2) == 0) Direction = true;
         else Direction = false;
+
+        Shadow = transform.GetChild(1).transform.gameObject;
+        Shadow.transform.position = new Vector3(Shadow.transform.position.x, GroundY, Shadow.transform.position.z);
+        ShadowYValue = Shadow.transform.position.y;
     }
 
     void Update()
     {
         transform.Translate(Vector3.down * DownSpeed * Time.deltaTime);
-        if(Direction)
+        Shadow.transform.Translate(Vector3.up * DownSpeed * Time.deltaTime);
+        if (Direction)
         {
             transform.Translate(Vector3.right * SideSpeed * Time.deltaTime);
         }
@@ -37,5 +58,6 @@ public class BubbleMovement : MonoBehaviour
         }
 
         if(transform.position.y < 0) Destroy(this.transform.gameObject);
+        Shadow.transform.position = new Vector3(Shadow.transform.position.x, ShadowYValue, Shadow.transform.position.z);
     }
 }
