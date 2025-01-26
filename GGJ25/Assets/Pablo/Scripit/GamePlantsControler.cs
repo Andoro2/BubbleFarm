8122a477369641beeb6f5afab2750a9ec33c63e5
Pabolt84 <>
2025-01-26 06:00:56 +0100

    new stuff

--- GGJ25/Assets/Pablo/Scripit/GamePlantsControler.cs
+++ GGJ25/Assets/Pablo/Scripit/GamePlantsControler.cs
@@ -9,16 +9,40 @@ public class GamePlantsControler : MonoBehaviour
     private int actuallife;
     public int Timer;
     private int time;
+    private bool pickupcrops;
+    private int pickcrop;
 
     private void Start()
     {
         actuallife = Gardenlifes;
         time = Timer;
+        pickupcrops = false;
+        pickcrop = 0;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (pickupcrops)
+        {
+            if (time<0)
+            {
+                plants[pickcrop].Estado = 0;
+                pickcrop++;
+                if (pickcrop >= plants.Count)
+                {
+                    pickcrop = 0;
+                    pickupcrops = false;
+                }
+                time = Timer;
+            }
+            else
+            {
+                time--;
+            }
+           
+        }
+
         int tmpFinish=0;
         for(int i = 0; i < plants.Count; i++)
         {
@@ -27,7 +51,7 @@ public class GamePlantsControler : MonoBehaviour
 
         if(tmpFinish/4 == plants.Count)
         {
-            //Juego superado
+            pickupcrops = true;
         }else if (tmpFinish <= 0)
         {
             if (time<0)
